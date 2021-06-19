c3cbf8c2534d33675e97490c86fe024d413a1f94
aryan465 <aryananand567@gmail.com>
2021-06-20 04:15:46 +0530

    UI 2.0, message init

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Main></Main>
-      <Auth></Auth>
+      {/* <Auth></Auth> */}
     </div>
   );
 }
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+
 import '../CSS/Main.css';
 import titlelogo from '../Logo/video-call (1).png';
 import chatlogo from '../Logo/chatlogo.png';
@@ -5,60 +7,85 @@ import search from '../Logo/search.png';
 import schedule from '../Logo/schedule.png';
 import send from '../Logo/send.png';
 import attach from '../Logo/attach.png';
-function Main(){
-    return(
-      <>
-        <header>
 
-          <img src= {titlelogo} alt="" />
-          <div className="name">Microsoft Teams</div>
 
-          <input type="text" placeholder="Search"/>
-        </header>
-        
-        <div className="window">
-          <div className="left">
-            <div className="chatlogo">
-              <img src={chatlogo} alt="" />
-              <h6>Chat</h6>
-            </div>
+function Main() {
+
+  const [message,setMessage] = useState('');
+
+  return (
+    <>
+      <header>
+
+        <img src={titlelogo} alt="" />
+        <div className="name">Microsoft Teams</div>
+
+        <input type="text" placeholder="Search" />
+      </header>
+
+      <div className="window">
+        <div className="left">
+          <div className="chatlogo">
+            <img src={chatlogo} alt="" />
+            <h6>Chat</h6>
           </div>
-          <div className="middle">
-            <div className="mhead">
-              <h3>Chat</h3>
-              <div className="logos">
-                <img src={search} alt="" />
-              </div>
-            </div>
-          </div>
-          <div className="right">
-            <div className="rhead">
-              <div className="meettitle">
-                <img src= {schedule} alt="" />
-                <h3 className="meetname">Meeting with Lorem Ipsum</h3>
-              </div>
-              <div className="files">
-                <ul>
-                  <li>Chat</li>
-                  <li>Files</li>
-                </ul>
-              </div>
-            </div>
-            <div className="rfoot">
-              <input type="text" placeholder="Type a new message.."/>
-              <div className="ficons">
-                <div className="attach">
-                  <img src={attach} alt="" />
-                </div>
-                <img className="send" src={send} alt="" 
-                  onClick={()=>console.log("hi!")}
-                />
-              </div>
+        </div>
+        <div className="middle">
+          <div className="mhead">
+            <h3>Chat</h3>
+            <div className="logos">
+              <img src={search} alt="" />
             </div>
           </div>
         </div>
-      </>
-    );
+        <div className="right">
+          <div className="rhead">
+            <div className="meettitle">
+              <img src={schedule} alt="" />
+              <h3 className="meetname">Meeting with Lorem Ipsum</h3>
+            </div>
+            <div className="files">
+              <ul>
+                <li>Chat</li>
+                <li>Files</li>
+              </ul>
+            </div>
+          </div>
+          <div className="rfoot">
+
+            <input className="msg" type="text" placeholder="Type a new message.." 
+              value = {message} onChange={(e)=>{
+                setMessage(e.target.value)
+              }}
+
+              onKeyDown = {(e)=>{
+                // console.log(e.key)
+                if(e.key === 'Enter'){
+                  (document.getElementById("send")).click()
+                }
+              }}
+            />
+
+            <div className="ficons">
+              <div className="attach">
+                <img src={attach} alt="" />
+              </div>
+
+              <img id="send" className="send" src={send} alt=""
+                onClick={()=>{
+                  
+                  // console.log(message);
+                  setMessage('');
+                  
+                }}
+              />
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Main;
