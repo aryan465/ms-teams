fea1c7b6618f6c58949d748e929815d9dd96a69b
aryan465 <aryananand567@gmail.com>
2021-06-25 02:23:24 +0530

    added email signup

---
+++ src/config/fbConfig.jsx
@@ -0,0 +1,20 @@
+import firebase from 'firebase';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+var firebaseConfig = {
+    apiKey: "***REMOVED***",
+    authDomain: "***REMOVED***",
+    projectId: "***REMOVED***",
+    storageBucket: "***REMOVED***.appspot.com",
+    messagingSenderId: "***REMOVED***",
+    appId: "1:***REMOVED***:web:cc6c31e2e5cb47707e5d8f",
+    measurementId: "***REMOVED***"
+  };
+  // Initialize Firebase
+  const fbapp = firebase.initializeApp(firebaseConfig);
+  firebase.firestore().settings({timestampsInSnapshots: true});
+
+
+export default fbapp;
