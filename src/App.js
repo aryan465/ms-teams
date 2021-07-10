67a6768b537efbfa11167ba5e3cfe792ed502d59
aryan465 <aryananand567@gmail.com>
2021-07-10 23:54:51 +0530

    fetchusers

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import Chatwindow from './Components/Chatwindow';
 import Start from './Components/Start';
 import SignIn from './Components/SignIn';
 import SignUp from './Components/SignUp';
-import { useState, useEffect } from 'react';
-import { auth } from './config/fbConfig';
-import { Redirect } from 'react-router';
 import { Route, Switch } from 'react-router-dom';
 
 
