efb81e83e4eaf3ac65821c8e6518cbb13d51ffae
aryan465 <aryananand567@gmail.com>
2021-07-12 15:49:32 +0530

    vc updates 2.0

--- src/Components/Webcall.jsx
+++ src/Components/Webcall.jsx
@@ -2,16 +2,22 @@ import '../CSS/web.css';
 import { auth, firestore } from '../config/fbConfig';
 import { useRef } from 'react';
 import video from '../Logo/video.png';
-import mic from '../Logo/mic.png';
+import microphone from '../Logo/mic.png';
 import endCall from '../Logo/endcall.png';
 import createCall from '../Logo/createcall.png';
 import { useState } from 'react';
+import { Redirect, useHistory } from 'react-router-dom';
+
 
 function Webcall() {
 
 
-  const[callId,setCallId] = useState("")
+  const [callId, setCallId] = useState("")
+  const [mystream, setMystream] = useState(null)
+  const [cameraOn ,setCamaeraon] = useState(true)
+  const [micOn ,setMicon] = useState(true)
 
+  let history = useHistory();
 
   const webcamVideo = useRef(null);
   const callButton = useRef(null);
@@ -21,15 +27,19 @@ function Webcall() {
   var localStream
   var remoteStream
 
+  if (auth.currentUser === null) {
+    return <Redirect to='/chat' />;
+
+  }
 
 
-      firestore.collection("users").doc(auth.currentUser.email).onSnapshot(
-      snapshot=>{
-        
-        const client = snapshot.data()["currentuser"]
-        const minClient = client.substring(0,client.indexOf("@"))
-        setCallId(snapshot.data()["mycalls"][minClient])
-      })
+  firestore.collection("users").doc(auth.currentUser.email).onSnapshot(
+    snapshot => {
+
+      const client = snapshot.data()["currentuser"]
+      const minClient = client.substring(0, client.indexOf("@"))
+      setCallId(snapshot.data()["mycalls"][minClient])
+    })
 
 
 
@@ -47,6 +57,8 @@ function Webcall() {
   async function webCam() {
     localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
     remoteStream = new MediaStream();
+
+    setMystream(localStream)
 
     // Push tracks from local stream to peer connection
     localStream.getTracks().forEach((track) => {
@@ -127,42 +139,47 @@ function Webcall() {
     hangupButton.current.disabled = false;
   };
 
-  async function answerButtonClick(me,minClient) {
+  async function answerButtonClick(me, minClient) {
 
+    const callDoc = firestore.collection('calls').doc(callId);
+    const answerCandidates = callDoc.collection('answerCandidates');
+    const offerCandidates = callDoc.collection('offerCandidates');
 
-      
-      console.log(callId)
-      const callDoc = firestore.collection('calls').doc(callId);
-      const answerCandidates = callDoc.collection('answerCandidates');
-      const offerCandidates = callDoc.collection('offerCandidates');
-  
-      pc.onicecandidate = (event) => {
-        event.candidate && answerCandidates.add(event.candidate.toJSON());
-      };
-      const callData = (await callDoc.get()).data();
-      console.log(callData)
-      const offerDescription = callData.offer;
-      await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
-  
-      const answerDescription = await pc.createAnswer();
-      await pc.setLocalDescription(answerDescription);
-  
-      const answer = {
-        type: answerDescription.type,
-        sdp: answerDescription.sdp,
-      };
-      await callDoc.update({ answer });
-  
-      offerCandidates.onSnapshot((snapshot) => {
-        snapshot.docChanges().forEach((change) => {
-          console.log(change);
-          if (change.type === 'added') {
-            let data = change.doc.data();
-            pc.addIceCandidate(new RTCIceCandidate(data));
-          }
-        });
+    pc.onicecandidate = (event) => {
+      event.candidate && answerCandidates.add(event.candidate.toJSON());
+    };
+    const callData = (await callDoc.get()).data();
+    const offerDescription = callData.offer;
+    await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
+
+    const answerDescription = await pc.createAnswer();
+    await pc.setLocalDescription(answerDescription);
+
+    const answer = {
+      type: answerDescription.type,
+      sdp: answerDescription.sdp,
+    };
+    await callDoc.update({ answer });
+
+    offerCandidates.onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change) => {
+        // console.log(change);
+        if (change.type === 'added') {
+          let data = change.doc.data();
+          pc.addIceCandidate(new RTCIceCandidate(data));
+        }
       });
+    });
   };
+
+  function muteMic() {
+    mystream.getAudioTracks().forEach(track => track.enabled = !track.enabled);
+  }
+
+  function muteCam() {
+    mystream.getVideoTracks().forEach(track => track.enabled = !track.enabled);
+  }
+
 
   return (
     <div className="vc" id="vc">
@@ -177,72 +194,128 @@ function Webcall() {
       </div>
 
 
-      <button id="callButton" ref={callButton}
-        onClick={() => {
-          console.log('click');
+      <div className="buttons">
+        <button id="callButton" ref={callButton}
+          onClick={() => {
+            document.getElementById("mic").classList.remove("disabled")
+            document.getElementById("camera").classList.remove("disabled")
 
-          const me = auth.currentUser.email;
-          const minMe = me.substring(0, me.indexOf("@"));
+            document.getElementById("callButton").disabled = true
+            document.getElementById("callButton").classList.add("disabled")
+            const me = auth.currentUser.email;
+            const minMe = me.substring(0, me.indexOf("@"));
 
-          firestore.collection("users").doc(me).get().
-            then(snapshot => {
-              const currentuser = snapshot.data()["currentuser"]
-              const minCurrentuser = currentuser.substring(0, currentuser.indexOf("@"));
+            firestore.collection("users").doc(me).get().
+              then(snapshot => {
+                const currentuser = snapshot.data()["currentuser"]
+                const minCurrentuser = currentuser.substring(0, currentuser.indexOf("@"));
 
-              var usercallId = snapshot.data()["mycalls"][minCurrentuser];
+                var usercallId = snapshot.data()["mycalls"][minCurrentuser];
 
-              if (usercallId === "") {
-                console.log(1)
-                webCam().then(()=>{
-                callButtonClick(me, minMe, currentuser, minCurrentuser);    
-                })
-              }
-
-              else {
-                console.log(2)
-                webCam().then(()=>{
-                  answerButtonClick(me,minCurrentuser);
-                })
-              }
-
-            })
-        }}
-      ><img src={createCall} alt="" /></button>
-
-
-      <button id="hangupButton" ref={hangupButton}
-        onClick={() => {
-          const me = auth.currentUser.email;
-          const minMe = me.substring(0, me.indexOf("@"));
-
-          firestore.collection("users").doc(me).get().
-            then(snapshot => {
-              const currentuser = snapshot.data()["currentuser"]
-              const minCurrentuser = currentuser.substring(0, currentuser.indexOf("@"));
-
-              let mycalls = snapshot.data()["mycalls"]
-              mycalls[minCurrentuser] = ""
-
-              firestore.collection("users").doc(me).update({
-                mycalls: mycalls
-              })
-
-              firestore.collection("users").doc(currentuser).get().
-                then(snap => {
-
-                  let clientcalls = snap.data()["mycalls"]
-                  clientcalls[minMe] = ""
-
-                  firestore.collection("users").doc(currentuser).update({
-                    mycalls: clientcalls
+                if (usercallId === "") {
+                  webCam().then(() => {
+                    callButtonClick(me, minMe, currentuser, minCurrentuser);
                   })
+                }
+
+                else {
+                  webCam().then(() => {
+                    answerButtonClick(me, minCurrentuser);
+                  })
+                }
+
+              })
+          }}
+        ><img src={createCall} alt="" /></button>
+
+        <button id="camera" className="disabled"
+          onClick={() => {
+            try {
+              muteCam();
+              if(cameraOn){
+              document.getElementById("camera").style.backgroundColor =  "rgb(247, 14, 33)"
+                setCamaeraon(false)
+              }
+              else{
+                document.getElementById("camera").style.backgroundColor =  "rgb(10, 123, 168)"
+                setCamaeraon(true)
+              }
+            }
+            catch (err) {
+              console.log(err)
+            }
+          }}>
+          <img src={video} alt="" />
+        </button>
+
+        <button id="mic" className="disabled"
+          onClick={() => {
+            try {
+              muteMic();
+              if(micOn){
+              document.getElementById("mic").style.backgroundColor =  "rgb(247, 14, 33)"
+                setMicon(false)
+              }
+              else{
+                document.getElementById("mic").style.backgroundColor =  "rgb(10, 123, 168)"
+                setMicon(true)
+              }
+            }
+            catch (err) {
+              console.log(err)
+            }
+          }}>
+          <img src={microphone} alt="" />
+        </button>
+
+        <button id="hangupButton" ref={hangupButton}
+          onClick={() => {
+            const me = auth.currentUser.email;
+            const minMe = me.substring(0, me.indexOf("@"));
+
+            firestore.collection("users").doc(me).get().
+              then(snapshot => {
+                const currentuser = snapshot.data()["currentuser"]
+                const minCurrentuser = currentuser.substring(0, currentuser.indexOf("@"));
+
+                let mycalls = snapshot.data()["mycalls"]
+                mycalls[minCurrentuser] = ""
+
+                firestore.collection("users").doc(me).update({
+                  mycalls: mycalls
                 })
+
+                firestore.collection("users").doc(currentuser).get().
+                  then(snap => {
+
+                    let clientcalls = snap.data()["mycalls"]
+                    clientcalls[minMe] = ""
+
+                    firestore.collection("users").doc(currentuser).update({
+                      mycalls: clientcalls
+                    })
+                  })
 
                 console.log("empty")
-            })
-        }}
-      >
-        <img src={endCall} alt="" /></button>
+
+              })
+
+            try {
+
+              mystream.getTracks().forEach(function (track) {
+                if (track.readyState == 'live') {
+                  track.stop();
+                  
+                }})
+            }
+            catch (err) {
+              console.log(err)
+            }
+            history.push("/chat")
+          }}
+        >
+          <img src={endCall} alt="" /></button>
+      </div>
     </div>
   );
 }
