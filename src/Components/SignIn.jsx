ca9eec21cf3e7bfdb07f5d76f0a0aae05593794e
aryan465 <aryananand567@gmail.com>
2021-07-13 23:16:01 +0530

    bug fixes

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -7,7 +7,6 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-// import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
@@ -31,7 +30,7 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: '100%', 
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -55,7 +54,6 @@ export default function SignIn() {
       auth.signInWithEmailAndPassword(email, password).then(()=>{
         
         history.push('/chat')
-        // console.log(auth.currentUser);
       });
 
     } catch (error) {
@@ -131,11 +129,7 @@ export default function SignIn() {
             Sign In
           </Button>
           <Grid container>
-            <Grid item xs>
-              <Link href="#" style={LinkStyle}>
-                Forgot password?
-              </Link>
-            </Grid>
+
             <Grid item>
               <Link to="/signup" style={LinkStyle}>
                 {"Don't have an account? Sign Up"}
